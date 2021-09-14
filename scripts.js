7aff6fae5c17ab42fa9c320a4c71239b12b87028
Sara <amiri_gs@hotmail.com>
2021-09-14 15:00:29 -0400

    initial commit to new branch

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Add a submit event listener to the form
+// Get data from user (name, email and comment)
+// Append the comment, user photo and date it was posted on the page
+
+let curDate = Date();
+
+console.log(curDate);
