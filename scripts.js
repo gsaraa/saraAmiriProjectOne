c34f56fe1dd44ca2f45c7fcec3e8cd83c8714dff
Sara <amiri_gs@hotmail.com>
2021-09-15 21:20:58 -0400

    edited stylesheet andhtml files

--- scripts.js
+++ scripts.js
@@ -4,30 +4,23 @@
     // Capture the value of username, email and comment inputs by using querySelectors and store them in a variable
 // Append the comment, user photo and date it was posted on the page
 
-
-// Storing the form element in a variable
 const formElement = document.querySelector('.formContainer');
 
 let date = new Date();
-// console.log(date);
 
-// let currentDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-// console.log(date.toLocaleString(undefined, currentDate));
+let currentDay = date.toLocaleString(undefined, {weekday: 'long'});
 
-currentDate = date.toLocaleString(undefined, {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+let currentDate = date.toLocaleString(undefined, {year: 'numeric', month: 'long', day: 'numeric'});
 
-console.log(currentDate);
+
 formElement.addEventListener('submit', function(event) {
     event.preventDefault();
     
     const userName = document.querySelector('#userName').value;
-    // console.log(userName);
     
     const userEmail = document.querySelector('#userEmail').value;
-    // console.log(userEmail);
     
     const userComment = document.querySelector('#userComment').value;
-    // console.log(userComment);
     
     const printUserComment = document.querySelector('.newUserComment');
     
@@ -35,8 +28,9 @@ formElement.addEventListener('submit', function(event) {
     <img src="./assets/default-user-picture.png" alt="a default picture of user leaving a comment">
     
     <div class="newComment">
-        <p class="commentTime">${currentDate} by ${userName}</p>
+        <p class="commentTime">${currentDay} ${currentDate} by ${userName}</p>
         <p>${userComment}</p>
     </div>`;
 
+    formElement.reset();
 });
